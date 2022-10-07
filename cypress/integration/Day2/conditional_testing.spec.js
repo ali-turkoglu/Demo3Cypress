1be55fb1c166fcf2b10ee08552c9b46614dd03ea
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-10-07 15:35:11 +0300

    conditional testing added

---
+++ cypress/integration/Day2/conditional_testing.spec.js
@@ -0,0 +1,28 @@
+
+
+describe('Conditional Testing',() =>{
+
+    it('Test wikovoyage exist using length',() =>{
+
+        cy.visit('/')
+
+        cy.get('body').then((body) => {
+
+            if(body.find('[class="other-project-title jsl10n"]').length > 0){ //it means if element is exist
+                cy.get('[data-jsl10n="wikivoyage.slogan"]').click()
+                cy.url().should('eq','https://www.wikivoyage.org/')
+            }else{
+                cy.get('[class="other-project-title jsl10n]').click()
+                cy.url().should('eq','https://commons.wikimedia.org/wiki/Main_Page')
+            }
+        })
+
+
+
+    })
+
+
+
+
+
+})
